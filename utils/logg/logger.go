ec878d49ce952d21541f299563c867cf528ea6a4
Mikhail Gladinov <gladinovmisha@gmail.com>
2026-04-22 12:12:56 +0300

    chore: add structured logging utilities

---
+++ utils/logg/logger.go
@@ -0,0 +1,43 @@
+package logg
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func NewLogger(env string) *slog.Logger {
+	var (
+		level   slog.Level
+		handler slog.Handler
+	)
+
+	switch env {
+	case envLocal:
+		level = slog.LevelDebug
+		base := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: level})
+		handler = New(base)
+
+	case envDev:
+		level = slog.LevelDebug
+		base := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: level})
+		handler = New(base)
+
+	case envProd:
+		level = slog.LevelInfo
+		base := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: level})
+		handler = New(base)
+
+	default:
+		level = slog.LevelInfo
+		base := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: level})
+		handler = New(base)
+
+	}
+	return slog.New(handler)
+}
