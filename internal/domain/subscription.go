3e69915ed9746754bb4d423af0772d03dda23723
Mikhail Gladinov <gladinovmisha@gmail.com>
2026-04-22 12:10:56 +0300

    feat: add subscription domain entity

---
+++ internal/domain/subscription.go
@@ -0,0 +1,41 @@
+package domain
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var ErrSubscriptionNotFound error = errors.New("not found subscription by this ID")
+
+type Subscription struct {
+	ID          uuid.UUID
+	ServiceName string
+	Price       int
+	UserID      uuid.UUID
+	StartDate   YearMonth
+	EndDate     *YearMonth
+}
+
+type YearMonth struct {
+	Year  int
+	Month time.Month
+}
+
+func (y *YearMonth) CountOfMonth() int {
+	return y.Year*12 + int(y.Month)
+}
+
+func (to *YearMonth) Sub(from YearMonth) int {
+	fromMonth := from.Year*12 + int(from.Month)
+	toMonth := to.Year*12 + int(to.Month)
+	return toMonth - fromMonth
+}
+
+type FilterTotal struct {
+	UserID      *uuid.UUID
+	ServiceName *string
+	From        *YearMonth
+	To          *YearMonth
+}
